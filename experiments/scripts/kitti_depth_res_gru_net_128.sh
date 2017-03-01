e11ad709c824159d06a5fce1b85419a9b300092f
Tomas Petricek <tpetricek@gmail.com>
2017-03-01 13:19:09 +0100

    Kitti vox 128x128 experiment script with configs.

---
+++ experiments/scripts/kitti_depth_res_gru_net_128.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# export CUDA_VISIBLE_DEVICES=
+export PYTHONUNBUFFERED="True"
+
+NET_NAME=KittiDepthResGRUNet
+EXP_DETAIL=kitti_depth_128
+OUT_PATH='./output/'$NET_NAME/$EXP_DETAIL
+LOG="$OUT_PATH/log.`date +'%Y-%m-%d_%H-%M-%S'`"
+
+CFG_TRN=experiments/cfgs/kitti_depth_trnval_128.yaml
+CFG_TST=experiments/cfgs/kitti_depth_tst_128.yaml
+
+# Make the dir if it not there
+mkdir -p $OUT_PATH
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+export THEANO_FLAGS="floatX=float32,device=gpu,assert_no_cpu_op='raise'"
+# export THEANO_FLAGS="floatX=float32,device=cpu,assert_no_cpu_op='raise'"
+
+python main.py \
+      --batch-size 1 \
+      --iter 25600 \
+      --out $OUT_PATH \
+      --model $NET_NAME \
+      --cfg $CFG_TRN \
+      ${*:1}
+
+echo 'Training completed. Running test net...'
+
+python main.py \
+      --test \
+      --batch-size 1 \
+      --out $OUT_PATH \
+      --weights $OUT_PATH/weights.npy \
+      --model $NET_NAME \
+      --cfg $CFG_TST \
+      ${*:1}
+
